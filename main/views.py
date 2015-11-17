7a8d9e4bdf739fa2564b7d2350cddc91cc0a00c8
Lei1121 <leiren1121@outlook.com>
2015-11-17 16:34:50 +0800

    init

---
+++ main/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import *
+
+# Create your views here.
+def fakeapple(request):
+    return render_to_response('fakeapple.html')
+
+def zhihu(request):
+    return render_to_response('zhihu.html')
+
+def fakemicrosoft(request):
+    return render_to_response('fakemicrosoft.html')
+
+def index(request):
+    return render_to_response('index.html')
+
+
